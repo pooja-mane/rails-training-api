0abee4e21045f4d8a98807c95cabfec546c9141e
Pooja Mane <pooja@joshsoftware.com>
2022-05-27 19:33:22 +0530

    Worked validating headers and auth token

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,28 @@
+class BaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :validate_header, :authenticate
+
+  def validate_header
+    return unless invalid_header
+
+    render_error(errors: [I18n.t('errors.invalid_headers')], status: :precondition_failed)
+  end
+
+  def authenticate
+    return unless request.headers['access-token'].blank?
+      
+    render_error(errors: I18n.t('errors.unauthorized'), status: precondition_failed)
+  end
+
+  def render_success(data: nil, message: nil, status: :ok)
+    render json: { data: data, message: message }, status: status
+  end
+
+  def render_error(errors: nil, status: :internal_server_error)
+    render json: { errors: errors }, status: status
+  end
+
+  def invalid_header
+    request.headers['client'].blank? || request.headers['uid'].blank?
+  end
+end
